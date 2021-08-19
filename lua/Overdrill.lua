43ecb49f1a26ab39af61681701967ca8d5fef842
Jamie McMillan <jamie.mcmillan@bbc.co.uk>
2021-08-19 13:35:58 +0100

    Refactor Overdrill.lua
     - Fix "Already activated" message
     - Do nothing on heists which aren't First World Bank

--- lua/Overdrill.lua
+++ lua/Overdrill.lua
@@ -6,29 +6,35 @@ function isLoud()
     return not managers.groupai:state():whisper_mode()
 end
 
-function isDeathWishOrDeathSentence()
+function isAtLeastDeathWish()
     return Global.game_settings.difficulty == "overkill_290" or Global.game_settings.difficulty == "sm_wish"
 end
 
-if not _overdrillActivated then
-    _overdrillActivated = true
-    if Network:is_server() and isFirstWorldBank() and isLoud() and isDeathWishOrDeathSentence() then
-        for _, script in pairs(managers.mission:scripts()) do
-            for id, element in pairs(script:elements()) do
-                for _, trigger in pairs(element:values().trigger_list or {}) do
-                    if trigger.notify_unit_sequence == "light_on" then
-                        element:on_executed()
-                        managers.chat:_receive_message(1, "Overdrill", "Ready to place the Overdrill", tweak_data.system_chat_color)
-                        RefreshTest()
-                    end
+function activateOverdrill()
+    for _, script in pairs(managers.mission:scripts()) do
+        for id, element in pairs(script:elements()) do
+            for _, trigger in pairs(element:values().trigger_list or {}) do
+                if trigger.notify_unit_sequence == "light_on" then
+                    element:on_executed()
+                    return
                 end
             end
         end
-    else
-        managers.chat:_receive_message(1, "Overdrill", "Conditions are not met!", Color.yellow)
-        managers.chat:_receive_message(1, "Requirements", "Hosting a lobby, Deathwish Loud", Color.yellow)
     end
-else
-    managers.chat:_receive_message(1, "Overdrill", "Already activated", Color.red)
-    RefreshTest()
+end
+
+if isFirstWorldBank() then
+    if not _overdrillActivated then
+        if Network:is_server() and isAtLeastDeathWish() and isLoud() then
+            activateOverdrill()
+            managers.chat:_receive_message(1, "Overdrill", "Ready to place the Overdrill", tweak_data.system_chat_color)
+            _overdrillActivated = true
+            RefreshTest()
+        else
+            managers.chat:_receive_message(1, "Overdrill", "Conditions are not met!", Color.yellow)
+            managers.chat:_receive_message(1, "Requirements", "Host, Deathwish+, Loud", Color.yellow)
+        end
+    else
+        managers.chat:_receive_message(1, "Overdrill", "Already activated", Color.red)
+    end
 end
